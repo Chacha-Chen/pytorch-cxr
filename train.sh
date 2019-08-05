2321a603f05d3fcc4f6d912079211fd57afb0e93
Jinserk Baik <jinserk.baik@gmail.com>
2019-08-05 11:19:52 -0400

    modify train.sh for single node, multiprocesses

--- train.sh
+++ train.sh
@@ -7,11 +7,11 @@ export MASTER_ADDR="127.0.0.1"
 export MASTER_PORT="23456"
 
 python -m torch.distributed.launch \
-  --nnodes 2 \
-  --node_rank 0 \
-  --nproc_per_node 1 \
+  --nproc_per_node 3 \
   --master_addr $MASTER_ADDR \
   --master_port $MASTER_PORT \
   train_noniid.py \
-  --cuda 0 \
-  --runtime-dir 20190710_20k_noniid_dist_partial_share
+  --cuda 0,1,2 \
+  --tensorboard \
+  --epoch 500 \
+  --runtime-dir 20190805_20k_noniid_dist_label_split
